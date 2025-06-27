2b8b57f612e55f6e5180df73093910316429eea8
Saul Campuzano <181676686+sauldevstuff@users.noreply.github.com>
2025-06-26 20:23:21 -0500

    Made the boxes change color to blue when mouse hovers over

--- script.js
+++ script.js
@@ -6,4 +6,7 @@ for (let i = 0; i < 256; i++) {
     const box = document.createElement("div");
     box.classList.add("box")
     container.append(box)
+    box.addEventListener("mouseover", e => {
+        box.style.backgroundColor = "blue";
+    })
 }
